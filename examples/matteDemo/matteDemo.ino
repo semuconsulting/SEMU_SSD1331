91a0894c4de84f64d542fa078fe6ab31213c89bf
Steve Smith <semuadmin@users.noreply.github.com>
2019-10-13 16:50:02 +0100

    matteDemo added

---
+++ examples/matteDemo/matteDemo.ino
@@ -0,0 +1,54 @@
+/***************************************************
+  SEMU_SSD1331 library matteDemo example
+
+  Illustrates how to use the drawImage() method to display transparents images
+  and matte ('green screen') fx.
+  
+  The image header files used in this demo were created using
+  Vladimir Riuson's lcd-image-convertor utility:
+  https://github.com/riuson/lcd-image-converter
+  Suitable presets and templates for use with this utility can be found here:
+  https://github.com/semuconsulting/SEMU_SSD1331/tree/master/lcd_image_convert_template
+
+ ****************************************************/
+//#define DEBUG
+
+#include <SEMU_SSD1331.h>
+#include "astronaut.h"
+#include "astronautmatte.h"
+#include "background.h"
+
+#define sclk 13   // marked SCL or CK on OLED board
+#define mosi 11   // marked SDA or SI on OLED board
+#define cs   10   // marked CS or OC on OLED board
+#define rst  9    // marked RES or R on OLED board
+#define dc   8    // marked DC or sometimes (confusingly) RS on OLED board
+
+#define PAUSE 2000
+
+SEMU_SSD1331 display = SEMU_SSD1331(&SPI, cs, dc, rst);
+
+void setup(void) {
+
+  display.begin();
+
+}
+
+void loop() {
+
+  // display the astronaut image without transparency
+  // image transparency is defined in the tImage struct as a boolean.
+  // (see the foot of the astronaut.h file)
+  display.drawImage(&astronaut);
+  delay(PAUSE);
+  
+  // display background image
+  display.drawImage(&background);
+  delay(PAUSE);
+  
+  // now overlay astronaut image but this time with the
+  // transparency flag in this image set to 'true'
+  display.drawMaskedImage(0,0, &background, &astronautmatte);
+  delay(PAUSE);
+
+}
